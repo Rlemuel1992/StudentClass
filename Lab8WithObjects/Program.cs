e1f490af9e80dcce273a4b26c38b6445bb15e3f9
Ronald Lemuel <Rlemuel1992@gmail.com>
2019-01-24 11:24:15 -0500

    First commit

---
+++ Lab8WithObjects/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentObject;
+
+namespace Lab8WithObjects
+{
+	class Program
+	{
+		static void Main()
+		{
+			Student s = new Student("Jimmy", "Hot dogs", "Detroit");
+			Student s2 = new Student("Laura", "Bibimbap", "New Mexico");
+			Student s3 = new Student("Kendra", "Veggie Burger + Fries", "Oak Park");
+			List<Student> students = new List<Student>
+			{
+				s,
+				s2,
+				s3,
+
+				new Student("Coleslaw Kendawg", "Peach Cobbler", "Heart of ATX")
+			};
+
+			//List<Student> students = new List<Student>();
+			//students.Add(s);
+			//students.Add(s2);			This is an alternative way for the above list. 
+			//students.Add(s3);
+
+			//students.Add(new Student("Coleslaw Kendawg", "Peach Cobbler", "Heart of ATX"));
+			Console.WriteLine("Please put in a number from 0 to " + (students.Count -1));
+			string input = Console.ReadLine();
+			int choice = int.Parse(input);
+
+			Student output = students[choice];
+			output.PrintInfo();
+		}
+	}
+}
---
+++ Lab8WithObjects/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentObject
+{
+	class Student
+	{
+		public string name;
+		public string favoriteFood;
+		public string homeTown;
+
+		public Student(string name, string favoriteFood, string homeTown)
+		{
+			this.name = name;
+			this.favoriteFood = favoriteFood;
+			this.homeTown = homeTown;
+		}
+		public void PrintInfo()
+		{
+			Console.WriteLine("Name: " + name);
+			Console.WriteLine("Hometown: " + homeTown);
+			Console.WriteLine("Favorite Food: " + favoriteFood);
+
+		}
+	}
+}
